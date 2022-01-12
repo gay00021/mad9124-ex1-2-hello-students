e849a38336949bedb5ce0b1fc26a8310f13dbb22
Thaw Thaw Wah <gay00021@algonquinlive.com>
2022-01-11 22:47:25 -0500

    add codes to js file

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,17 @@
 'use strict'
 
+
 // 1. Read the JSON file into a variable called students
+let students = require('./students.json')
+// console.log(students) 
 
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
+students.forEach(({firstName, lastName}) => {
+    console.log(`Hello ${firstName} ${lastName}`)
+})
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+let result = students.filter(({lastName}) => lastName.startsWith('D'))
+console.log(`Count of last names starting with D is ${result.length}`)
